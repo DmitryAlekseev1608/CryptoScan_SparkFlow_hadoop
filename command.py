08ecd7ff26c2994a663dc5484a21f8f8e3aa40f3
Dmitry <a89652251224@gmail.com>
2023-12-09 01:24:55 +0300

    Work version

--- command.py
+++ command.py
@@ -1,7 +1,5 @@
 import os
-
 from time import sleep
-
 import pyspark
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
@@ -9,46 +7,24 @@ from pyspark.streaming import StreamingContext
 from api.parsing import parsing
 import pandas as pd
 from pyspark.sql import SQLContext
-import multiprocessing
-
-m = multiprocessing.Manager()
-QUEUE_RDD = m.list()
-
-def get_date(sqlContext):
-    
-    QUEUE_RDD = []
-    while True:
-
-        df = parsing()
-        spDF = sqlContext.createDataFrame(data = df)
-        rdd = spDF.rdd
-        QUEUE_RDD += [rdd]
-
-def start_spark(ssc):
-
-    ssc.start()
-    sleep(5)
-    ssc.awaitTermination()
+import tqdm
 
 def main():
-    
-    QUEUE_RDD = []
-    sc = SparkContext("local[*]", "cryptoscan")
-    ssc = StreamingContext(sc, 5)
-    sqlContext = SQLContext(sc)
-    df = parsing()
-    spDF = sqlContext.createDataFrame(data = df) 
-    rdd = spDF.rdd
-    QUEUE_RDD += [rdd]
-    inputStream = ssc.queueStream(QUEUE_RDD)
-    inputStream.pprint()
 
-    p1 = multiprocessing.Process(target=get_date, args=[sqlContext])
-    p1.start()
-    p2 = multiprocessing.Process(target=start_spark, args=[ssc])
-    p2.start()
-    p1.join()
-    p2.join()
+    while True:
+        sc = SparkContext("local[*]", "cryptoscan")
+        ssc = StreamingContext(sc, 3)      
+        queue_rdd = []      
+        sqlContext = SQLContext(sc)
+        for _ in tqdm.tqdm(range(3)):
+            df = parsing()
+            spDF = sqlContext.createDataFrame(data = df)
+            rdd = spDF.rdd
+            queue_rdd += [rdd]
+        inputStream = ssc.queueStream(queue_rdd)
+        inputStream.pprint()
+        ssc.start()
+        ssc.stop(stopSparkContext=True, stopGraceFully=True)
 
 if __name__ == "__main__":
     main()
