077679d0d779ebe2ca0ff10e82d50100ab0d0c47
Dmitry <a89652251224@gmail.com>
2023-12-08 23:48:45 +0300

    Work

--- command.py
+++ command.py
@@ -1,32 +1,53 @@
 import os
-import time
+
+from time import sleep
 
 import pyspark
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.streaming import StreamingContext
-
 from api.parsing import parsing
+import pandas as pd
+from pyspark.sql import SQLContext
+import multiprocessing
 
+m = multiprocessing.Manager()
+QUEUE_RDD = m.list()
+
+def get_date(sqlContext):
+
+    QUEUE_RDD = []
+    while True:
+
+        df = parsing()
+        spDF = sqlContext.createDataFrame(data = df)
+        rdd = spDF.rdd
+        QUEUE_RDD += [rdd]
+
+def start_spark(ssc):
+
+    ssc.start()
+    ssc.awaitTermination()
 
 def main():
+    
+    QUEUE_RDD = []
+    sc = SparkContext("local[*]", "cryptoscan")
+    ssc = StreamingContext(sc, 5)
+    sqlContext = SQLContext(sc)
     df = parsing()
-    sdf = df.to_spark()
-    sdf.show(10)
-    d = sdf.format("json").save(d)
-    q = spark.readStream.schema("age INT, name STRING").format("json").load(d).writeStream.format("console").start()
-    time.sleep(3)
-    q.stop()
+    spDF = sqlContext.createDataFrame(data = df) 
+    rdd = spDF.rdd
+    QUEUE_RDD += [rdd]
+    inputStream = ssc.queueStream(QUEUE_RDD)
+    inputStream.pprint()
 
-    # # Create a local StreamingContext with two working thread and batch interval of 1 second
-    # sc = SparkContext("local[*]", "cryptoscan")
-    # ssc = StreamingContext(sc, 30)
-
+    p1 = multiprocessing.Process(target=get_date, args=[sqlContext])
+    p1.start()
+    p2 = multiprocessing.Process(target=start_spark, args=[ssc])
+    p2.start()
+    p1.join()
+    p2.join()
 
 if __name__ == "__main__":
-    spark = SparkSession.builder.appName("spark3.2show").getOrCreate()
-    print("Spark info :")
-    print("Version of pyspark :", pyspark.__version__)
-    os.environ["PYARROW_IGNORE_TIMEZONE"] = "1"
-
     main()
