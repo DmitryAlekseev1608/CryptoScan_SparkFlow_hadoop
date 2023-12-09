8dd0331668b431cb4e58e5654cd3b747accf8137
Dmitry <a89652251224@gmail.com>
2023-12-09 20:25:47 +0300

    END

--- command.py
+++ command.py
@@ -1,30 +1,32 @@
-import os
-from time import sleep
-import pyspark
-from pyspark import SparkContext
-from pyspark.sql import SparkSession
-from pyspark.streaming import StreamingContext
-from api.parsing import parsing
+from multiprocessing import Process
+
 import pandas as pd
-from pyspark.sql import SQLContext
+import pyspark
 import tqdm
+from pyspark import SparkContext
+from pyspark.sql import SparkSession, SQLContext
+from pyspark.streaming import StreamingContext
+
+from api.parsing import parsing
+
+
+def start_spark():
+    sc = SparkContext("local[*]", "cryptoscan")
+    ssc = StreamingContext(sc, 3)
+    inputStream = ssc.textFileStream("temp")
+    inputStream.pprint()
+    ssc.start()
+    ssc.awaitTermination()
+
 
 def main():
+    p_pars = Process(target=parsing)
+    p_spark = Process(target=start_spark)
+    p_pars.start()
+    p_spark.start()
+    p_pars.join()
+    p_spark.join()
 
-    while True:
-        sc = SparkContext("local[*]", "cryptoscan")
-        ssc = StreamingContext(sc, 3)      
-        queue_rdd = []      
-        sqlContext = SQLContext(sc)
-        for _ in tqdm.tqdm(range(3)):
-            df = parsing()
-            spDF = sqlContext.createDataFrame(data = df)
-            rdd = spDF.rdd
-            queue_rdd += [rdd]
-        inputStream = ssc.queueStream(queue_rdd)
-        inputStream.pprint()
-        ssc.start()
-        ssc.stop(stopSparkContext=True, stopGraceFully=True)
 
 if __name__ == "__main__":
     main()
