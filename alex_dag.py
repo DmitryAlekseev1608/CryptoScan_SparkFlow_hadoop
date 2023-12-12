6075fb6b584c675a55c0b5c354d48f6c17d53d87
Dmitry <a89652251224@gmail.com>
2023-12-12 19:27:00 +0000

    init Airflow

--- alex_dag.py
+++ alex_dag.py
@@ -5,8 +5,8 @@ import datetime as dt
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 import command
-import compute.current
-import compute.days
+# import compute.current
+# import compute.days
 
 default_args = {
     'owner': 'alex',
@@ -15,15 +15,15 @@ default_args = {
     'retry_delay': dt.timedelta(minutes=5),
 }
 
-with DAG('cryptoscan',
+with DAG('alex_cryptoscan',
          default_args=default_args
          ) as dag:
 
     air_command = PythonOperator(task_id='delivery_hadoop',
                                  python_callable=command.main)
     
-    air_current = PythonOperator(task_id='current_best_in_telegram',
-                                 python_callable=compute.current.main)
+    # air_current = PythonOperator(task_id='current_best_in_telegram',
+    #                              python_callable=compute.current.main)
     
-    air_current = PythonOperator(task_id='all_price_in_prometheus',
-                                 python_callable=compute.days.main)
+    # air_days = PythonOperator(task_id='all_price_in_prometheus',
+    #                              python_callable=compute.days.main)
